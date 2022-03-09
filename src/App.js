fd37b2ee79beb1055f02b848a57415133d40c4db
Rohit Rambade <rohitrambade18@gmail.com>
2022-03-09 22:44:59 +0530

    Basic UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+
+import AddContact from './AddContact';
 import './App.css';
+import ContactList from './ContactList';
+import Header from './Header';
+
 
 function App() {
+  const contacts=[
+    {
+      id:"1",
+      name:"rohit",
+      email:"rohitrambade19@gmail.com"
+
+    },
+    {
+      id:"2",
+      name:"satish",
+      email:"satish@gmail.com"
+
+    }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='ui container'>
+      <Header/>
+      <AddContact/>
+      <ContactList contacts={contacts}/>
     </div>
   );
 }
